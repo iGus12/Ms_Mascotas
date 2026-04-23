ebb03077828c6167e6db0ee80b004b4f9180154d
iGus12 <ag.varas@duocuc.cl>
2026-04-23 09:03:09 -0400

    Add files via upload

---
+++ src/main/java/Mascotas/Ms/Macotas/Application.java
@@ -0,0 +1,13 @@
+package Mascotas.Ms.Macotas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
---
+++ src/test/java/Mascotas/Ms/Macotas/ApplicationTests.java
@@ -0,0 +1,13 @@
+package Mascotas.Ms.Macotas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
